74c15d686083f352d72429a5e08a58176da8cdae
soonnambin <soonnam.bin@rsquare.co.kr>
2023-02-09 19:28:48 +0900

    회원가입 화면 접근 추가

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -19,7 +19,6 @@ describe('로그인 화면', () => {
     cy.get('[href="/login/emailrecovery"]').click();
 
     cy.get('.design-cache-prefix-33fpmi').contains('아이디 찾기');
-   
     
   });
 
@@ -30,8 +29,17 @@ describe('로그인 화면', () => {
     cy.get('[href="/login/passwordrecovery"]').click();
 
     cy.get('.design-cache-prefix-33fpmi').contains('비밀번호 찾기');
+      
+  });
+
+  it('회원가입 화면 접근', () => {
+
+    cy.get('[href="/login"]').click();
+
+    cy.get('.design-cache-prefix-19bc87t').click();
+
+    cy.get('.design-cache-prefix-2q4gog').contains('약관 동의');
    
-    
   });
 
 })
